781b9d0d0696d40950f3e2de6c61a863662be9d2
towhid046 <towhidmorol46@gmail.com>
2024-02-12 22:28:28 +0600

    make make common function to do the same thing again and again

--- script/traiangle.js
+++ script/traiangle.js
@@ -1,13 +1,73 @@
+const display = document.getElementById("aside");
+
 function getInputValue(inputId) {
   const value = parseFloat(document.getElementById(inputId).value);
   document.getElementById(inputId).value = "";
-  return value;
+    return value;
 }
 
-function traingleCalculator() {
-  const traingleBase = getInputValue("traingle-base");
-  const traingleHeight = getInputValue("traingle-height");
-  console.log(traingleBase, traingleHeight);
-  const area = 0.5 * traingleBase * traingleHeight;
-  document.getElementById("aside").innerText = `Traingle Area: ${area}`;
+function triangleCalculator() {
+  const base = getInputValue("traingle-base");
+  const height = getInputValue("traingle-height");
+  const area = 0.5 * base * height;
+  if(area >= 0){
+    display.innerHTML = `<p>Traingle Area: ${area} cm<sup>2</sup></sup></p>`;
+  } else{
+    alert('Invalid Input')
+  }
+}
+
+function rectangleCalculator() {
+  const base = getInputValue("rectangle-base");
+  const height = getInputValue("rectangle-height");
+  const area = base * height;
+  if(area >= 0){
+    display.innerHTML = `<p>Rectangle Area: ${area} cm<sup>2</sup></sup></p>`;
+  } else{
+    alert('Invalid Input')
+  }
+}
+
+function parallelogramCalculator() {
+  const base = getInputValue("parallelogram-base");
+  const height = getInputValue("parallelogram-height");
+  const area = base * height;
+  if(area >= 0){
+    display.innerHTML = `<p>Parallelogram Area: ${area} cm<sup>2</sup></sup></p>`;
+  } else{
+    alert('Invalid Input')
+  }
+}
+
+function rhombusCalculator() {
+  const base = getInputValue("rhombus-base");
+  const height = getInputValue("rhombus-height");
+  const area = 0.5 * base * height;
+  if(area >= 0){
+    display.innerHTML = `<p>Rhombus Area: ${area} cm<sup>2</sup></sup></p>`;
+  } else{
+    alert('Invalid Input')
+  }
+}
+
+function pentagonCalculator() {
+  const base = getInputValue("pentagon-base");
+  const height = getInputValue("pentagon-height");
+  const area = 0.5 * base * height;
+  if(area >= 0){
+    display.innerHTML = `<p>Pentagon Area: ${area} cm<sup>2</sup></sup></p>`;
+  } else{
+    alert('Invalid Input')
+  }
+}
+
+function ellipseCalculator() {
+  const base = getInputValue("ellipse-base");
+  const height = getInputValue("ellipse-height");
+  const area = (Math.PI).toFixed(3) * base * height;
+  if(area >= 0){
+    display.innerHTML = `<p>Ellipse Area: ${area} cm<sup>2</sup></sup></p>`;
+  } else{
+    alert('Invalid Input')
+  }
 }
